827a9f71f5842ba61d5ed887e84d0f91852d4ffe
chhaviram <chhaviram7890@EMAIL.com>
2019-11-12 14:57:26 +0530

    Pushing Chhaviram system's

---
+++ db/migrate/20191112072533_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :image
+      t.string :description
+      t.date :dat
+
+      t.timestamps
+    end
+  end
+end
