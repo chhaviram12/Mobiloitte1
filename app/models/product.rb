f99b93fde8095b347529aa9089e793e0c99092a6
Pankaj <Pankaj@gmail.com>
2019-11-14 14:32:10 +0530

    valid

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }, :format => { :with => /\A\w+\z/ }
   validates :description, presence: true, length: { maximum: 100 , minimum:10}
   validates :dat, presence: true
-  validates_format_of :image, :with => %r{\.(png|jpg|jpeg)}i, :message => "only .jpg ang .png"
+  validates_format_of :image,presence: true, :with => %r{\.(png|jpg|jpeg)}i, :message => "only .jpg .jpej ang .png"
 
 
 
