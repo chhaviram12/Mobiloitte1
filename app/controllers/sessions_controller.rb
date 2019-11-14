271b501ff60e86da5f2efeca82feda5947da7052
Saurabh <Saurabh@gmail.com>
2019-11-14 11:24:11 +0530

    stying

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,10 +19,11 @@ end
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
     else
-      flash.now[:alert] = "Email or password is invalid"
+      flash.now[:error] = "Email or Password is invalid"
       render "new"
     end
   end
+
   def destroy
     session[:user_id] = nil
     redirect_to root_url, notice: "Logged out!"
